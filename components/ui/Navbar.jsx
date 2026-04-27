44c4051d615e89307d757bc079f478dd67821111
Sadikun <sadikun1768@gmail.com>
2026-04-27 17:23:58 +0600

    up

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -21,34 +21,37 @@ export default function Navbar() {
 
                 {/* menu */}
 
-                <NavigationMenu>
+              <NavigationMenu className="hidden lg:flex">
                     <NavigationMenuList>
-                        <NavigationMenuItem>
-                            <NavigationMenuLink href="/news">News</NavigationMenuLink>
+                        <NavigationMenuItem className="flex items-center space-x-3">
+                            <NavigationMenuLink href="/news" className="hover:text-cyan-500 font-bold">News</NavigationMenuLink>
                         </NavigationMenuItem>
                         <NavigationMenuItem>
-                            <NavigationMenuTrigger href="/services">Services</NavigationMenuTrigger>
+                            <NavigationMenuTrigger href="/services" className="hover:text-cyan-500 font-bold">Services</NavigationMenuTrigger>
                             <NavigationMenuContent>
                                 <ul>
-                                    <li><NavigationMenuLink href="/services/web-design">Web Design</NavigationMenuLink></li>
-                                    <li><NavigationMenuLink href="/services/development">Development</NavigationMenuLink></li>
-                                    <li><NavigationMenuLink href="/services/mobile-app">Mobile App</NavigationMenuLink></li>
-                                    <li><NavigationMenuLink href="/services/seo">Seo</NavigationMenuLink></li>
+                                    <li><NavigationMenuLink href="/services/web-design" className="hover:text-cyan-500 font-bold">Web Design</NavigationMenuLink></li>
+                                    <li><NavigationMenuLink href="/services/development" className="hover:text-cyan-500 font-bold">Development</NavigationMenuLink></li>
+                                    <li><NavigationMenuLink href="/services/mobile-app" className="hover:text-cyan-500 font-bold">Mobile App</NavigationMenuLink></li>
+                                    <li><NavigationMenuLink href="/services/seo" className="hover:text-cyan-500 font-bold">Seo</NavigationMenuLink></li>
                                 </ul>
                             </NavigationMenuContent>
                         </NavigationMenuItem>
                            <NavigationMenuItem>
-                            <NavigationMenuLink href="/news">About</NavigationMenuLink>
+                            <NavigationMenuLink href="/about" className="hover:text-cyan-500 font-bold">About</NavigationMenuLink>
                         </NavigationMenuItem>
                            <NavigationMenuItem>
-                            <NavigationMenuLink href="/news">Contact</NavigationMenuLink>
+                            <NavigationMenuLink href="/contact" className="hover:text-cyan-500 font-bold">Contact</NavigationMenuLink>
                         </NavigationMenuItem>
                            <NavigationMenuItem>
-                            <NavigationMenuLink href="/news">LogIn</NavigationMenuLink>
+                            <NavigationMenuLink href="/login" className="hover:text-cyan-500 font-bold">LogIn</NavigationMenuLink>
                         </NavigationMenuItem>
                     </NavigationMenuList>
-                </NavigationMenu>
+                </NavigationMenu> 
 
+
+
+                
 
 
                 {/* color mode and button */}
