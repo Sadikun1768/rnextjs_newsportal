30d77e3735fdbde79bd86145af2a7079081fda6d
Sadikun <sadikun1768@gmail.com>
2026-05-16 13:31:04 +0600

    News/recepi List added

--- app/news/page.js
+++ app/news/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 function page() {
   return (
     <div className='py-6'>
-      <h2 className='text-black font-bold'>Latest News</h2>
+      <h2 className='text-2xl font-bold'>Latest News</h2>
 
       <NewsList />
       
--- components/news/NewsList.jsx
+++ components/news/NewsList.jsx
@@ -1,21 +1,39 @@
 "use client"
 
 import { useState, useEffect } from "react";
+import NewsCard from "../ui/NewsCard";
+import getRecipes from "@/lib/fetchNews";
 
 export default function NewsList() {
   
-    const [posts, setPosts] = useState([]);
     const [search, setSearch] = useState("");
-    const [category, setCategory] = useState();
+    const [recipes, setRecipes] = useState([]);
+    const [category, setCategory] = useState("");
     
 
     useEffect(() => {
-
-    }, [])
+        const fetchData = async () => {
+            const response = await getRecipes(category, search);
+            setRecipes(response.recipes);
+        
+        }
+      
+        fetchData();
+    },[category, search]);
+  
+// console.log(data.recipes);
     
   return (
     <div>
+
         <h3>News List</h3>
+
+     {recipes.map((recipe) => (
+
+       <NewsCard key={recipe.id} recipe={recipe} />
+
+      ))}
+
 
 
     </div>
