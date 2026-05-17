d13b904d4fd46429a553a3e9ac1f8caa22f1f567
Sadikun <sadikun1768@gmail.com>
2026-05-17 18:31:13 +0600

    up

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function AboutPage() {
+export default function AboutPage(params, searchParams) {
   return (
     <div>About Page</div>
   )
---
+++ context/themecontext.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function themecontext() {
+  return (
+    <div>themecontext</div>
+  )
+}
